a2472db6be47856c82e7661b9ed8921c7ae3bb9d
esteban95-a <dpachuri@cesde.net>
2025-09-27 14:19:01 -0500

    Fase 3: Añadida simulación de ventas y diagnóstico de inventario

--- DIDIER-ESTEBAN-ACHURI-1035870854/app.js
+++ DIDIER-ESTEBAN-ACHURI-1035870854/app.js
@@ -21,3 +21,30 @@ const reponerStock = (cantidadRepuesta) => {
 registrarVenta(20);
 reponerStock(30);
 mostrarInventario();
+const simulardiadeventas = () => {
+    for (let i = 1; i < 5; i++) {
+        console.log(`cliente ${i}`);
+        registrarVenta(8);
+    }
+};
+function diagnosticoDeInventario() {
+    if (stockproducto >= 50) {
+            console.log("nivel de stock optimo");
+    }else if (stockproducto <= 40) {
+        console.log("stock moderado , considere reponer pronto");
+    }else {
+        console.log(" alerta bajo stock , reposicion urgente");
+    }
+    switch (ventastotales) {
+        case 40:
+            console.log("producto estrella , alta demanda");
+            break;
+        case 30:
+            console.log("ventas moderadas");
+            break;
+        case 20:
+            console.log("baja rotacion del producto");
+    }
+}
+simulardiadeventas();
+diagnosticoDeInventario();
