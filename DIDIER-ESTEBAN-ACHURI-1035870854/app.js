a804a412f1f9ea735f2a6307ec50954cc6fc33bc
esteban95-a <dpachuri@cesde.net>
2025-09-27 13:31:03 -0500

    Fase 2: Implementadas funciones para ventas y reposición de stock

--- DIDIER-ESTEBAN-ACHURI-1035870854/app.js
+++ DIDIER-ESTEBAN-ACHURI-1035870854/app.js
@@ -1,7 +1,23 @@
 let nombreproducto = prompt("ingrese nombre de producto");
-const stockproducto = 50;
-const ventastotales = 0;
+let stockproducto = 50;
+let ventastotales = 0;
 function mostrarInventario() {
     console.log("producto: " + nombreproducto + " stock: " + stockproducto+" unidades" + " ventas: " + ventastotales+" unidades");
 }
+
+const registrarVenta = function(cantidadVendida) {
+  if (cantidadVendida <= stockproducto) {
+    stockproducto -= cantidadVendida;
+    ventastotales += cantidadVendida;
+    console.log(`Éxito`);
+  } else {
+    console.log(" Stock insuficiente para esta venta.");
+  }
+};
+const reponerStock = (cantidadRepuesta) => {
+  stockproducto += cantidadRepuesta;
+  console.log(` Se añadieron ${cantidadRepuesta} unidades.\nNuevo stock: ${stockproducto}.`);
+};
+registrarVenta(20);
+reponerStock(30);
 mostrarInventario();
